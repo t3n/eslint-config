12a7cf16e84e1bc1f70a73cd2cea26a0728f7954
Philip Plagge <sayhi@itscursedphil.com>
2019-09-09 11:40:47 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react'
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+    'no-console': 0,
+    'no-nested-ternary': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.jsx', 'tsx']
+      }
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0
+  }
+};
---
+++ typescript.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  rules: {
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true
+      }
+    ]
+  }
+};
