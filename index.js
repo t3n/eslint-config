31eabe9dea530ba9b065bef23fffae952dda7cff
Johannes Steu <hello@johannessteu.de>
2019-12-09 19:52:05 +0100

    Disable eslint/import rule

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   rules: {
     'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
     'import/prefer-default-export': 0,
     'no-console': 0,
     'no-nested-ternary': 0,
