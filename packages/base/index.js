ca12332ab06cd8f970805e650101b851e0fdec34
Philip Plagge <plaggep@gmail.com>
2021-04-29 09:52:43 +0200

    Add sorting plugin and custom dependency sorting logic

--- packages/base/index.js
+++ packages/base/index.js
@@ -4,6 +4,7 @@ module.exports = {
     'plugin:jest/recommended',
     'plugin:prettier/recommended',
   ],
+  plugins: ['simple-import-sort'],
   env: {
     browser: true,
     node: true,
@@ -24,5 +25,26 @@ module.exports = {
     'import/prefer-default-export': 1,
     'no-console': 1,
     'no-nested-ternary': 1,
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Side effect imports.
+          ['^\\u0000'],
+          // Packages.
+          // React, then things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^react', '^@?\\w'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ['^'],
+          // @†3n namespace packages
+          ['^@t3n'],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
   },
 };
